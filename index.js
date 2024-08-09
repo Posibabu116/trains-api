7ab34dc51412f7c887e11c92190fcf7097cba0d8
Posibabu116 <yallaposibabu117@gmail.com>
2024-08-09 10:15:32 +0530

    hiii

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/api/trains', (req, res) => {
   res.json(trainData);
 });
 
+app.get('/api', (req, res) => {
+  res.json({success:true});
+});
+
 const PORT = 8080;
 app.listen(PORT, '192.168.0.12', () => {
   console.log(`Server running on http://192.168.0.12:${PORT}`);
